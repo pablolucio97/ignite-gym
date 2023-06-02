508ebb84af0dda26cd1e7a5a902e391fd83d8f56
pablolucio97 <pablolucio_@hotmail.com>
2023-06-02 13:32:51 -0300

    styles: configured native base theme

--- App.tsx
+++ App.tsx
@@ -1,26 +1,24 @@
 import { Text, View, StatusBar } from 'react-native';
+import { NativeBaseProvider } from 'native-base'
 import {
   useFonts,
   Roboto_400Regular,
   Roboto_700Bold
 } from '@expo-google-fonts/roboto'
-
+import { Spinner } from '@components/Spinner';
+import { THEME } from './src/themes'
 export default function App() {
 
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
 
   return (
-    <View
-      style={{
-        backgroundColor: '#202024'
-      }}
-    >
+    <NativeBaseProvider theme={THEME}>
       <StatusBar
         translucent
         barStyle='light-content'
         backgroundColor='transparent'
       />
       {fontsLoaded ? <Text>Open up App.tsx to start working on your app!</Text> : <View />}
-    </View>
+    </NativeBaseProvider>
   );
 }
---
+++ src/components/Spinner/index.tsx
@@ -0,0 +1,10 @@
+import { Spinner as SpinnerComponent } from 'native-base'
+
+export function Spinner() {
+    return (
+        <SpinnerComponent
+            size={32}
+            color='green.500'
+        />
+    )
+}
